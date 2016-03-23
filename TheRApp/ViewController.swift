68d735f62e7a480825bc64d335ce67be86134a93
Marshall Cuico <marshallcuico@Marshalls-MacBook-Pro.local>
2016-03-22 18:16:01 -0700

    First prototype of project finished.

--- TheRApp/ViewController.swift
+++ TheRApp/ViewController.swift
@@ -14,14 +14,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var text: UILabel!
     @IBOutlet weak var volume: UISlider!
     @IBOutlet weak var image: UIImageView!
-    @IBOutlet weak var skip: UISlider!
+    @IBOutlet weak var skipSlider: UISlider!
     
     var musicPlayer: AVAudioPlayer!
+    var lyricArray = ["Yo", "sup", "player", "hello", "rap", "this", "song"]
+    var x = 0
+    var timer = NSTimer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         initAudio()
+        updateSkipBar()
+        updateLycrics()
         
     }
     
@@ -34,36 +39,66 @@ class ViewController: UIViewController {
             musicPlayer.prepareToPlay()
             musicPlayer.numberOfLoops = -1
             musicPlayer.play()
+            
+            skipSlider.maximumValue = Float(musicPlayer.duration)
+            
         } catch let err as NSError {
             print(err.debugDescription)
         }
         
+    }
+    
+    func updateLabel() {
+        if x != lyricArray.count {
+            let lyric = lyricArray[x]
+            x++
+            text.text = lyric
+        } else {
+            x = 0
+        }
+        
+    }
+    
+    func updateLycrics() {
+        timer = NSTimer.scheduledTimerWithTimeInterval(3, target: self, selector: Selector("updateLabel"), userInfo: nil, repeats: true)
+    }
+    
+    func updateSkipBar() {
+        _ = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("updateSlider"), userInfo: nil, repeats: true)
+    }
+    
+    func updateSlider() {
+        skipSlider.value = Float(musicPlayer.currentTime)
     }
 
     @IBAction func playBtnPressed(sender: UIButton!) {
     
         if musicPlayer.playing {
             musicPlayer.stop()
+            timer.invalidate()
             sender.alpha = 0.2
         } else {
             musicPlayer.play()
+            updateLycrics()
             sender.alpha = 10.0
         }
     
     }
     
     @IBAction func forwardBtnPressed(sender: AnyObject) {
+        initAudio()
     }
     
     @IBAction func backBtnPressed(sender: AnyObject) {
-        initAudio()s
+        initAudio()
     }
     
     @IBAction func volumeMoved(sender: AnyObject) {
-            musicPlayer.volume = volume.value
+        musicPlayer.volume = volume.value
     }
     
     @IBAction func skipSliderMoved(sender: AnyObject) {
+        musicPlayer.currentTime = NSTimeInterval(skipSlider.value)
     }
     
     
